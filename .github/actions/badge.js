c7cadfe40ee9e5c002095ac203e048552112b1b4
Kevin Camós Soto <kevincamossoto@gmail.com>
2021-12-15 03:59:55 +0100

    README: prueba

--- .github/actions/badge.js
+++ .github/actions/badge.js
@@ -1,46 +1,31 @@
 const core = require('@actions/core')
 const fs = require("fs");
 
-// const github = require('@actions/github')
-/* const TelegramBot = require('node-telegram-bot-api');
- */
 const resultado_test = core.getInput('resultado_test')
 
 var succes= "https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg"
 var fail =  "https://img.shields.io/badge/test-failure-red";
 console.log(resultado_test)
 
-
 try {
-
     const readme = "./README.md";
     var content = resultado_test != "failure" ? succes:fail;
     content =`RESULTADOS DE LOS ÚLTIMOS TEST: ![Image text](${content})`
     fs.readFile(readme, 'utf8', function (err, data) {
         data = data.split("<!-- RESULTADO -->")
 
-        data = data[0]+ "\n<!-- RESULTADO -->\n"+content+"\n<!-- RESULTADO -->\n"+ data[2];
-        
-        
+        data = data[0]+ "\n<!-- RESULTADO -->\n"+content+"\n<!-- RESULTADO -->\n"+ data[2]; 
         fs.writeFile(readme, content, function (err, result) {
             if (err) console.log('error', err);
         });
     });
     core.setOutput = ("frase-de-prueba resultado_test", resultado_test)
 
-
 } catch (error) {
     var content =  "failure";
-
-
     fs.readFile(readme, 'utf8', function (err, data) {
-
         data = data.split("<!-- RESULTADO -->")
-
         data = data[0]+ "\n<!-- RESULTADO -->\n"+content+"\n<!-- RESULTADO -->\n"+ data[2];
-        
-        
-
         fs.writeFile(readme, data, function (err, result) {
             if (err) console.log('error', err);
         });
--- .github/actions/discord.js
+++ .github/actions/discord.js
@@ -1,11 +1,9 @@
 const core = require('@actions/core')
 const discord_token = core.getInput('discord_token')
 const id_channel = core.getInput('id_channel')
-// const commit_author = core.getInput('commit_author')
-// const commit_committer = core.getInput('commit_committer')
-// const commit_message = core.getInput('commit_message')
-
-
+const commit_author = core.getInput('commit_author')
+const commit_committer = core.getInput('commit_committer')
+const commit_message = core.getInput('commit_message')
 
 
 const { Client, Intents } = require("discord.js");
@@ -14,26 +12,16 @@ const client = new Client({
 });
 client.on("ready", () => {
   console.log(`Logged in as ${client.user}!`);
-  // ${commit_author}
-  // ${commit_committer}
-  // ${commit_message}
+
   client.channels
     .fetch(id_channel)
     .then((channel) => channel.send(`
-    COMMIT AUTHOR:    
-    COMMIT COMMITTER: 
-    COMMIT MESSAGE:   
-    
+    COMMIT AUTHOR:    ${commit_author}
+    COMMIT COMMITTER:     ${commit_committer}
+    COMMIT MESSAGE:       ${commit_message}
     
     `))
     .catch((err) => console.log("Could not find the channel."));
-});
-
-client.on("messageCreate", (message) => {
-  console.log(message.channel);
-  if (message.author.bot) return false;
-  message.channel.send("Ei");
-  //   console.log(`Message from ${message.author.username}: ${message.content}`);
 });
 
 client.login(discord_token);
